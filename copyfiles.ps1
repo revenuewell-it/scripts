950551ad415b3066d2b010567f38c70161e87293
revenuewell-it <80802050+revenuewell-it@users.noreply.github.com>
2021-03-16 23:16:08 -0500

    Update copyfiles.ps1

--- copyfiles.ps1
+++ copyfiles.ps1
@@ -3,5 +3,5 @@ mkdir C:\HWID\IntuneScripts
 mkdir C:\HWID\IntuneScripts\Autopilot
 mkdir C:\HWID\IntuneScripts\ReadBatchRMGPnSCCM
 
-Copy-Item "Autopilot.ps1" -Destination "C:\HWID\IntuneScripts\Autopilot"
-Copy-Item "ReadBatchRMGPnSCCM.ps1" -Destination "C:\HWID\IntuneScripts\ReadBatchRMGPnSCCM"
+Copy-Item "https://raw.githubusercontent.com/revenuewell-it/scripts/main/Autopilot.ps1" -Destination "C:\HWID\IntuneScripts\Autopilot"
+Copy-Item "https://raw.githubusercontent.com/revenuewell-it/scripts/main/ReadBatchRMGPnSCCM.ps1" -Destination "C:\HWID\IntuneScripts\ReadBatchRMGPnSCCM"
